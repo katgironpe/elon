49bc9903f1f6b438c1fec7b50e09c51d4518656e
Katherine G. Pe <katherinepe@me.com>
2016-09-02 21:57:56 +0800

    Setup

---
+++ lib/elon.rb
@@ -0,0 +1,58 @@
+require 'hanami/model'
+require 'hanami/mailer'
+Dir["#{ __dir__ }/elon/**/*.rb"].each { |file| require_relative file }
+
+Hanami::Model.configure do
+  ##
+  # Database adapter
+  #
+  # Available options:
+  #
+  #  * Memory adapter
+  #    adapter type: :memory, uri: 'memory://localhost/elon_development'
+  #
+  #  * SQL adapter
+  #    adapter type: :sql, uri: 'sqlite://db/elon_development.sqlite3'
+  #    adapter type: :sql, uri: 'postgres://localhost/elon_development'
+  #    adapter type: :sql, uri: 'mysql://localhost/elon_development'
+  #
+  adapter type: :sql, uri: ENV['ELON_DATABASE_URL']
+
+  ##
+  # Migrations
+  #
+  migrations 'db/migrations'
+  schema     'db/schema.sql'
+
+  ##
+  # Database mapping
+  #
+  # Intended for specifying application wide mappings.
+  #
+  # You can specify mapping file to load with:
+  #
+  # mapping "#{__dir__}/config/mapping"
+  #
+  # Alternatively, you can use a block syntax like the following:
+  #
+  mapping do
+    # collection :users do
+    #   entity     User
+    #   repository UserRepository
+    #
+    #   attribute :id,   Integer
+    #   attribute :name, String
+    # end
+  end
+end.load!
+
+Hanami::Mailer.configure do
+  root "#{ __dir__ }/elon/mailers"
+
+  # See http://hanamirb.org/guides/mailers/delivery
+  delivery do
+    development :test
+    test        :test
+    # production :stmp, address: ENV['SMTP_PORT'], port: 1025
+  end
+end.load!
